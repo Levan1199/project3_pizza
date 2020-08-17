67a1c828b9f482a6cffb1a07f4976850cbb32e1e
Levan1199 <vipchicken28@gmail.com>
2020-08-17 10:10:22 +0700

    Finish menu models

--- orders/models.py
+++ orders/models.py
@@ -10,5 +10,46 @@ class RegularPizza(models.Model):
     def __str__(self):
         return f"{self.name} - {self.small_price} - {self.large_price}"
 
-class SicilianPizza(models.Model,RegularPizza):
-    pass
+class SicilianPizza(models.Model):
+    name = models.CharField(max_length=64)
+    small_price = models.FloatField()
+    large_price = models.FloatField()
+
+    def __str__(self):
+        return f"{self.name} - {self.small_price} - {self.large_price}"
+
+class Toppings(models.Model):
+    name = models.CharField(max_length=64)    
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Subs(models.Model):
+    name = models.CharField(max_length=64)
+    small_price = models.FloatField()
+    large_price = models.FloatField()
+
+    def __str__(self):
+        return f"{self.name} - {self.small_price} - {self.large_price}"
+
+class Pasta(models.Model):
+    name = models.CharField(max_length=64)
+    price = models.FloatField()   
+
+    def __str__(self):
+        return f"{self.name} - {self.price}"
+
+class Salads(models.Model):
+    name = models.CharField(max_length=64)
+    price = models.FloatField()   
+
+    def __str__(self):
+        return f"{self.name} - {self.price}"
+
+class DinnerPlatters(models.Model):
+    name = models.CharField(max_length=64)
+    small_price = models.FloatField()
+    large_price = models.FloatField()
+
+    def __str__(self):
+        return f"{self.name} - {self.small_price} - {self.large_price}"
